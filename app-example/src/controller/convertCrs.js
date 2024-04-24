17035aa6abe4f59465caa2f7440797f0d7a76d72
rifqiace <rifsentosa@gmail.com>
2024-04-24 18:46:50 +0200

    Replace proj4.defs with const

--- app-example/src/controller/convertCrs.js
+++ app-example/src/controller/convertCrs.js
@@ -1,15 +1,17 @@
-const defineCrs = require("../config/crs");
 const reproject = require("reproject");
 const proj4 = require("proj4").default;
+const { EPSG28992, EPSG4326 } = require("../config/crs.js");
 
 function convertCRS(geojson) {
-  defineCrs();
+  // const EPSG28992 = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
+  // const EPSG4326 = "+proj=longlat +datum=WGS84 +no_defs"
+
   geojson.features.forEach((feature) => {
     const geometry = feature.geometry;
     if (geometry && geometry.coordinates) {
       geometry.coordinates = geometry.coordinates.map((polygon) =>
         polygon.map((ring) =>
-          ring.map((vertex) => proj4("EPSG:28992", "EPSG:4326", vertex))
+          ring.map((vertex) => proj4(EPSG28992, EPSG4326, vertex))
         )
       );
     }
